f0415b378f4bffe4739e394fd0fa45968a74c718
Pawn Production <nathan.prospert@pawnproduction.fr>
2018-01-26 22:43:29 +0100

    Player behavior + player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public PlayerMovement pM;
+
+    private void Start()
+    {
+        pM = GetComponent<PlayerMovement>();
+    }
+
+    private void FixedUpdate()
+    {
+        
+        if(Input.GetAxis("Horizontal") > 0) // RIGHT
+        {
+            pM.MoveRight();
+        }
+        else if(Input.GetAxis("Horizontal") < 0) // LEFT
+        {
+            pM.MoveLeft();
+        }
+        else if(Input.GetAxis("Vertical") > 0)
+        {
+            pM.MoveUp();
+        }
+        else if(Input.GetAxis("Vertical") < 0)
+        {
+            pM.MoveDown();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 1.0f;
+
+
+    public void MoveLeft()
+    {
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, 180, transform.localEulerAngles.z);
+        Move();
+    }
+
+    public void MoveRight()
+    {
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, 0, transform.localEulerAngles.z);
+        Move();
+    }
+
+    public void MoveUp()
+    {
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, -90, transform.localEulerAngles.z);
+        Move();
+    }
+
+    public void MoveDown()
+    {
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, 90, transform.localEulerAngles.z);
+        Move();
+    }
+
+    public void Move()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+}
