245dc5db040ce9c4499fe23bab6a2ea517b28675
Argetkill\HarleyDav <flo.pouyer@hotmail.fr>
2018-01-27 14:10:51 +0100

    add backpack for student

---
+++ Assets/Scripts/student/backPackScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backPackScript : MonoBehaviour {
+    public Collider2D backPackCollider;
+    public SpriteRenderer backPackRenderer;
+    // Use this for initialization
+    void Start () {
+        backPackCollider = gameObject.GetComponent<BoxCollider2D>();
+        backPackRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void toggleBackpack()
+    {
+        backPackCollider.enabled = ! backPackCollider.enabled;
+        backPackRenderer.enabled = !backPackRenderer.enabled;
+    }
+}
