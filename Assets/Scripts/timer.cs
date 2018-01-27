9ce2fe7af00600a3b58bc482ef03f9892f49ebb7
pierrebigaud <pierrebigaud44@orange.fr>
2018-01-27 07:49:08 +0100

    creation timer pierreBigaud

---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+
+    // pivot, objet pour faire tourner l'aiguille
+    // hourtext -> text du temps
+    // timepasse temps passe
+    // timeEnd, quand le temps est passe
+    public GameObject pivot;
+    public Text hourText;
+    public float speedTimer;
+    public float timePasse;
+    public bool timeEnd;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timePasse<9.5)
+        {
+            hourText.text = "heure : 11:0" + System.Convert.ToInt32(timePasse);
+        }
+        else
+        {
+            hourText.text = "heure : 11:" + System.Convert.ToInt32(timePasse);
+        }
+        if (timePasse<=60)
+        {
+            timePasse += Time.deltaTime * speedTimer / 6;
+            pivot.transform.Rotate(new Vector3(0, 0, -(speedTimer * Time.deltaTime)));
+        }
+        else
+        {
+            timeEnd = true;
+        }
+	}
+}
